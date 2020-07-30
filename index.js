3367eb0eed2448ae5f95666e57c524cbabdf624e
acaine03 <60111377+acaine03@users.noreply.github.com>
2020-07-29 20:53:21 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+import { Header, Nav, Main, Footer } from "./components";
+import * as state from "./store";
+import * as components from "./components";
+import Navigo from 'navigo';
+
+
+const router = new Navigo(window.location.origin);
+
+router
+.on({
+    ":page": params => render(state[(params.page)]),
+    "/": () => render(state.HomePage)
+})
+.resolve();
+
+function render(st) {console.log(st)
+    document.querySelector("#root").innerHTML = `
+    ${Header(st)}
+    ${Nav(state.Links)}
+    ${Main(st)}
+    ${Footer()}
+    `;
+
+}
+
+router.updatePageLinks();
+
+
+
