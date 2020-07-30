e9c19659f73730263c99c1d762ca9b5f65421461
acaine03 <60111377+acaine03@users.noreply.github.com>
2020-07-29 21:34:47 -0500

    Add credit for font family.

--- components/Footer.js
+++ components/Footer.js
@@ -2,6 +2,7 @@ export default () => `
 <footer>
     <div class="footer">
         <h6>&copy; 2020 Perfectly Imperfect Counseling LLP</h6>
+        <a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>
     </div>
 </footer>
 `;
