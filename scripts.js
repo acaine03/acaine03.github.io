f50c4b4444cbab9ab8e5cbf26765f91ed82046e8
acaine03 <60111377+acaine03@users.noreply.github.com>
2020-07-29 21:08:42 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+import { Header, Nav, Main, Footer } from "./components";
+import * as state from "./store";
+
+function render(st = state.HomePage) {
+  document.querySelector("#root").innerHTML = `
+  ${Header(st)}
+  ${Nav(state.Links)}
+  ${Main(st)}
+  ${Footer()}
+`;
+  addNavEventListeners();
+
+}
+
+render();
+
+function addNavEventListeners() {
+  // add event listeners to Nav items for navigation
+  document.querySelectorAll("nav a").forEach(navLink =>
+    navLink.addEventListener("click", event => {
+      event.preventDefault();
+      render(state[event.target.title]);
+    })
+  )
+};
